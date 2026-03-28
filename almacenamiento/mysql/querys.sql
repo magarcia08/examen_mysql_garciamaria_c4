70dda5415f2fd01997b8ab05612fbfe7e09bf733
magarcia08 <mariagarcia36508@gmail.com>
2026-03-27 21:13:33 -0500

    entrega

--- almacenamiento/mysql/querys.sql
+++ almacenamiento/mysql/querys.sql
@@ -25,3 +25,24 @@ SELECT t.nombre  AS titular,
 FROM sustituciones s
 JOIN medicos t  ON s.id_medico_titular   = t.id_medico
 JOIN medicos s2 ON s.id_medico_sustituto = s2.id_medico;
+
+
+-- 5. Número de médicos que están actualmente en sustitución.
+SELECT m.nombre, m.apellido
+FROM medicos m
+JOIN sustituciones s ON m.id_medico = s.id_medico_sustituto
+WHERE CURDATE() BETWEEN s.fecha_inicio AND s.fecha_fin;
+
+
+-- 6. Horas totales de consulta por médico por día de la semana.
+SELECT m.nombre, m.apellido, h.hora_inicio, h.hora_fin
+FROM medicos m
+JOIN horarios_consulta h ON m.id_medico = h.id_medico
+WHERE h.dia_semana = 'Lunes';
+
+
+-- 7. Médico con mayor cantidad de pacientes asignados.
+SELECT m.nombre, m.apellido, COUNT(p.id_paciente) AS total_pacientes
+FROM medicos m
+LEFT JOIN paciente p ON m.id_medico = p.id_medico_asignado
+GROUP BY m.id_medico;
